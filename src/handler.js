ec2763c456711a7de4e093f5eff57a9fccb724f9
Michael Elian Kevin <michaelkevin47@gmail.com>
2022-11-04 15:29:38 +0700

    feat: implement Criteria 4
    API can update saved book.

--- src/handler.js
+++ src/handler.js
@@ -87,10 +87,10 @@ const getAllBooksHandler = () => {
 };
 
 const getBookByIdHandler = (request, h) => {
-  const {id} = request.params;
-  const book = books.filter((book) => book.id === id)[0];
+  const {bookId} = request.params;
+  const book = books.filter((book) => book.id === bookId)[0];
 
-  if (book == undefined) {
+  if (book === undefined) {
     const response = h.response({
       status: 'fail',
       message: 'Buku tidak ditemukan',
@@ -106,4 +106,75 @@ const getBookByIdHandler = (request, h) => {
   return response;
 };
 
-module.exports = {addBookHandler, getAllBooksHandler, getBookByIdHandler};
+const editBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak ' +
+      'boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+  }
+
+  const updatedAt = new Date().toISOString();
+
+  books[index] = {
+    ...books[index],
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    updatedAt,
+  };
+
+  const response = h.response({
+    status: 'success',
+    message: 'Buku berhasil diperbarui',
+  });
+  response.code(200);
+  return response;
+};
+
+module.exports = {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+};
